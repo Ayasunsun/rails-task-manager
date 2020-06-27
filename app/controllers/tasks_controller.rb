1ca0fc1ff638753c102f57fb4ab173f872407e51
Ayako Amano <ayakoamano21@gmail.com>
2020-06-27 16:00:01 +0900

    finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :show, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -14,25 +15,32 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
 
   def update
-    @task = Task.find(params[:id])
+
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
+
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
